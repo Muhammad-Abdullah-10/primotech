171270605ebd702efef99abb1a1b691c490b98ea
Muhammad-Abdullah-10 <59797973+Muhammad-Abdullah-10@users.noreply.github.com>
2024-04-26 13:00:10 +0500

    slick slider js with vertical scroll and on mouse wheel added

--- inc/js/index.js
+++ inc/js/index.js
@@ -1,3 +1,4 @@
+
 // const swiper = new Swiper('.swiper', {
 //     speed: 400,
 //     spaceBetween: 100,
@@ -7,8 +8,9 @@
 // import { gsap } from "gsap";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.from(".navbar-nav", { opacity: 0, y: '-100%', duration: 1 ,ease : 'back.in'});
-gsap.from(".logo-desktop", { opacity: 0, x: '-100%', duration: 1 ,ease : 'elastic.in'});
+gsap.from(".logo-desktop", { opacity: 0, x: '-100%', duration: 1 ,ease : 'power1'});
 gsap.from(".nav-item" , { duration: 1 , opacity: 0 , delay : 1 ,stagger : .2})  
+
 
 // let tween = gsap.to(".navbar-nav", { x: 100 }),
 //   st = ScrollTrigger.create({
@@ -18,7 +20,7 @@ gsap.from(".nav-item" , { duration: 1 , opacity: 0 , delay : 1 ,stagger : .2})
 //     animation: tween,
 //   });
 
-console.log(st.animation); // tween
+// console.log(st.animation); // tween
 // Swiper Working
 //   var quoteSwiper = new Swiper('.swiper');
 
@@ -29,7 +31,53 @@ console.log(st.animation); // tween
 
 //   quoteSwiper.params.control = imageSwiper;
 //   imageSwiper.params.control = quoteSwiper;
+
+    // $(".slider").slick({});
+    // $(".slider2").slick({ asNavFor: '.slider' });
+    // $(".slider3").slick({ asNavFor: '.slider2' });
 // Swiper Working
 
 // swiper.slideNext();
 // swiper.speed();
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots:false,
+    arrows:false,
+    draggable:true,
+    swipe: true,
+    touchMove: true,
+    vertical: true,
+    verticalScrolling: true,
+    focusOnSelect: true,
+    variableWidthL:true,
+    initialSlide:3,
+    verticalSwiping:true
+  });
+//   onmousewheel
+  const sliderNav = $(".slider-nav");
+  sliderNav.on('wheel', (function(e) {
+    e.preventDefault();
+  
+    if (e.originalEvent.deltaY < 0) {
+      $(this).slick('slickNext');
+    } else {
+      $(this).slick('slickPrev');
+    }
+  }));
+  //   onmousewheel
+ 
+  $('a[data-slide]').click(function(e) {
+    e.preventDefault();
+    var slideno = $(this).data('slide');
+    $('.slider-nav').slick('slickGoTo', slideno - 1);
+  });
