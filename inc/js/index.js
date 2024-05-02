c282d22f991237627c2162532dd32be69eb8a4be
Muhammad-Abdullah-10 <59797973+Muhammad-Abdullah-10@users.noreply.github.com>
2024-05-02 12:29:17 +0500

    Banner Video Section Completed All css is loading from _homaBanner not customcss

--- inc/js/index.js
+++ inc/js/index.js
@@ -1,19 +1,95 @@
 
-// const swiper = new Swiper('.swiper', {
-//     speed: 400,
-//     spaceBetween: 100,
-//     allowSlideNext : true,
-//   });
-       
 // import { gsap } from "gsap";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
-import LocomotiveScroll from 'node_modules/locomotive-scroll';
+// import LocomotiveScroll from 'node_modules/locomotive-scroll';
+      
+document.addEventListener("DOMContentLoaded", function () {
 
 gsap.from(".navbar-nav", { opacity: 0, y: '-100%', duration: 1 ,ease : 'back.in'});
 gsap.from(".logo-desktop", { opacity: 0, x: '-100%', duration: 1 ,ease : 'power1'});
 gsap.from(".nav-item" , { duration: 1 , opacity: 0 , delay : 1 ,stagger : .2})  
 
+var verticalSwiper = new Swiper(".services-thumb-slider", {
+  slidesPerView: 5,
+  speed: 400,
+  direction: "vertical",
+  centeredSlides: true,
+  mousewheel: true,
+  initialSlide: "2",
+});
+verticalSwiper.on("slideChange", function () {
+  updateValueOnSlideChange(this);
+});
 
+// Initialize horizontal swiper
+var horizontalSwiper = new Swiper(".swiper-horizontal", {
+  slidesPerView: 1,
+  initialSlide: "2",
+  speed: 400,
+  centeredSlides: true,
+  effect: "fade",
+  fadeEffect: { crossFade: false },
+});
+horizontalSwiper.on("slideChange", function () {
+  myCallbackfunction(this);
+});
+
+//Callback function used for when slide is changed.
+function myCallbackfunction(data) {
+  var activeSlide = verticalSwiper.slides[verticalSwiper.realIndex];
+  // var anchorText = activeSlide.querySelector("data-slide");
+  var anchorTexta = activeSlide.querySelector("a");
+  var a = anchorTexta.getAttribute('data-slide');
+  // console.log(a);
+  // console.log(data.realIndex);
+  var activeSlide = horizontalSwiper.slides[data.realIndex];
+  // console.log(activeSlide);
+  // var attr = activeSlide.find('.wrapper-content-slider');
+  // console.log(attr);
+}
+function updateValueOnSlideChange(data) {
+  var activeSlide = verticalSwiper.slides[verticalSwiper.realIndex];
+  var anchorText = activeSlide.querySelector("a");
+  var getBannerImgDiv = document.querySelector(".swipper-wrapper");
+
+  var classMap = {
+    "Web Development": "webiste-banner-bg",
+    "Mobile Application Development": "mobile-dev-banner",
+    "Digital Marketing": "digital-marketing-banner",
+    "Digital Branding & Communication": "digital-branding-banner",
+    "UI/UX Design": "ui-ux-banner",
+    "Ecommerce Web Development": "e-com-banner"
+};
+
+// Loop through the classMap and add or remove classes based if anchor text is
+// to the classMap text then add class coresponding to text
+Object.entries(classMap).forEach(([text, className]) => {
+    if (anchorText.textContent === text) {
+        getBannerImgDiv.classList.add(className);
+    } else {
+        getBannerImgDiv.classList.remove(className);
+    }
+});
+}
+
+// Synchronize slides when vertical swiper changes
+// verticalSwiper.on("slideChange", function () {
+//   var activeIndex = verticalSwiper.activeIndex;
+//   horizontalSwiper.slideTo(activeIndex);
+// });
+
+// Synchronize slides when horizontal swiper changes
+// horizontalSwiper.on("slideChange", function () {
+//   var activeIndex = horizontalSwiper.activeIndex;
+//   verticalSwiper.slideTo(activeIndex);
+// });
+
+// Connection b/w both sliders
+horizontalSwiper.controller.control = verticalSwiper;
+verticalSwiper.controller.control = horizontalSwiper;
+// Swiper Home Page Service Slider End
+
+// Test Code
 // let tween = gsap.to(".navbar-nav", { x: 100 }),
 //   st = ScrollTrigger.create({
 //     trigger: ".trigger",
@@ -81,3 +157,14 @@ gsap.from(".nav-item" , { duration: 1 , opacity: 0 , delay : 1 ,stagger : .2})
   // var swiperleft = document.getElementsByClassName('swiper-slide-active');
   // console.log(swiperleft);
 //  activeIndex.getAttribute('data-val');
+
+
+
+  // Swiper Home Page Service Slider Start
+  // Initialize vertical swiper
+// Test Code
+
+});
+
+
+  
